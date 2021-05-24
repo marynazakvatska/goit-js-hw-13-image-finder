de59088a48493825dbb01999f8e4920ed53e3b39
marynazakvatska <maryna.zakvatska@gmail.com>
2021-05-25 00:05:32 +0200

    no scroll

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import "./styles.css";
 import pictureCardTmpl from "./cardTmpl.hbs";
 import ApiService from "./apiService";
-
 const apiService = new ApiService();
-/* console.log(apiService); */
-
+console.log(apiService);
 document.body.insertAdjacentHTML(
   "afterbegin",
   ` <form class="search-form" id="search-form">
@@ -16,65 +14,63 @@ document.body.insertAdjacentHTML(
   />
 </form>`
 );
-
 const refs = {
   searchForm: document.querySelector(".search-form"),
   /* 
   list: document.querySelector("ul"), */
 };
-
 const listEl = document.createElement("ul");
 listEl.classList.add("gallery");
 document.body.append(listEl);
-
 const loadMoreBtn = document.createElement("button");
 loadMoreBtn.classList.add("loadbtn");
-
 loadMoreBtn.textContent = "Load more";
 document.body.append(loadMoreBtn);
-
 refs.searchForm.addEventListener("submit", onSearch);
 loadMoreBtn.addEventListener("click", () => {
   apiService.fetchPicture().then((hits) => {
     picMarkup(hits);
+    /*   window.scrollTo({
+      top: document.documentElement.offsetHeight,
+      behavior: "smooth",
+    }); */
   });
-  /* loadMoreBtn.scrollIntoView({
+  /* window.scrollTo({
+    top: document.documentElement.offsetHeight,
     behavior: "smooth",
-    block: loadMoreBtn,
   }); */
 });
 
 function onSearch(e) {
   e.preventDefault();
-
   console.log(e.target);
-  apiServiсe.query = e.currentTarget.elements.query.value;
-
-  if (apiService.query === "") {
-    return alert("Add something...");
+  apiService.searchQuery = e.currentTarget.elements.query.value;
+  if (apiService.searchQuery === "") {
+    return alert("Add something...please");
   }
-
   apiService.resetPage();
   apiService.fetchPicture().then((hits) => {
     clearCardList();
     picMarkup(hits);
   });
-}
-
-/* function onLoadMore() {
-  apiService.fetchPicture().then((hits) => {
-    picMarkup(hits);
+  window.scrollTo({
+    top: 150,
+    behavior: "smooth",
   });
-} */
-/* 
-window.scrollTo({
-  top: document.documentElement.scrollHeight,
-  behavior: "smooth",
-}); */
+}
 
 function picMarkup(hits) {
   listEl.insertAdjacentHTML("beforeend", pictureCardTmpl(hits));
+  /*   window.scrollTo({
+    top: listEl.scrollHeight,
+    behavior: "smooth",
+  }); */
+  /* window.scrollTo({
+    top: document.documentElement.offsetHeight,
+    behavior: "smooth",
+  }); */
 }
+
 function clearCardList() {
   listEl.innerHTML = "";
 }
